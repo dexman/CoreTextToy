21f58909ddf4bf5ff3671290fd33bbc7c4aeefed
Arthur Dexter <arthur.dexter@livingsocial.com>
2016-06-09 19:56:12 -0500

    Adds Carthage support.

---
+++ Test/Pods/Target Support Files/CoreTextToy/CoreTextToy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CoreTextToy : NSObject
+@end
+@implementation PodsDummy_CoreTextToy
+@end
---
+++ Test/Pods/Target Support Files/CoreTextToy/CoreTextToy-umbrella.h
@@ -0,0 +1,20 @@
+#import <UIKit/UIKit.h>
+
+#import "CCoreTextAttachment.h"
+#import "CCoreTextLabel.h"
+#import "CCoreTextLabel_HTMLExtensions.h"
+#import "CCoreTextRenderer.h"
+#import "CLinkingCoreTextLabel.h"
+#import "CMarkupValueTransformer.h"
+#import "CSimpleHTMLParser.h"
+#import "NSAttributedString_Extensions.h"
+#import "NSCharacterSet_HTMLExtensions.h"
+#import "NSScanner_HTMLExtensions.h"
+#import "NSString_HTMLExtensions.h"
+#import "UIColor+Hex.h"
+#import "UIFont_CoreTextExtensions.h"
+#import "UIImage+AnimatedConveniences.h"
+
+FOUNDATION_EXPORT double CoreTextToyVersionNumber;
+FOUNDATION_EXPORT const unsigned char CoreTextToyVersionString[];
+
